256ea68acf6f3318f0461410d3dd7ff54dc06b52
May Thu Htet <maythuhtet2892@gmail.com>
2024-09-10 15:28:55 +0100

    binary-search-continue

---
+++ binary-search/35_Search_Insert_Position.js
@@ -0,0 +1,55 @@
+// 35. Search Insert Position
+
+// Given a sorted array of distinct integers and a target value, return the index if
+// the target is found. If not, return the index where it would be if it were inserted
+// in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+// Example 1:
+
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2
+// Example 2:
+
+// Input: nums = [1,3,5,6], target = 2
+// Output: 1
+// Example 3:
+
+// Input: nums = [1,3,5,6], target = 7
+// Output: 4
+
+// Constraints:
+
+// 1 <= nums.length <= 104
+// -104 <= nums[i] <= 104
+// nums contains distinct values sorted in ascending order.
+// -104 <= target <= 104
+
+// Solution
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+//  serch the target using binary search
+//  if found, return the index of that target
+//  if not found, return the index of target at which it should be
+var searchInsert = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return left;
+};
