6075c3ae1ee0d573fd2c1402715a0692907b87b6
May Thu Htet <maythuhtet2892@gmail.com>
2024-09-29 17:43:27 +0100

    380 edited

--- array and string/380_Insert Delete GetRandom O(1).js
+++ array and string/380_Insert Delete GetRandom O(1).js
@@ -1,41 +1,73 @@
-class RandomizedSet {
-  constructor() {
-    this.map = new Map(); // Map to store value -> index mapping
-    this.arr = []; // Array to store the values for random access
+// Initialize the RandomizedSet object
+var RandomizedSet = function () {
+  // We use a map to store the value-to-index mapping for quick lookup
+  this.map = new Map();
+  // We use an array to store the values for constant-time random access
+  this.arr = [];
+};
+
+/**
+ * Inserts an item val into the set if not present.
+ * Returns true if the item was not present, false otherwise.
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.insert = function (val) {
+  // If the value is already present in the set (map), return false
+  if (this.map.has(val)) {
+    return false;
   }
+  // Otherwise, insert the value into the array and map
+  this.arr.push(val); // Add the value to the end of the array
+  this.map.set(val, this.arr.length - 1); // Map the value to its index in the array
+  return true; // Return true to indicate the value was successfully inserted
+};
 
-  // Inserts a value into the set if not present
-  insert(val) {
-    if (this.map.has(val)) {
-      return false; // If value exists, return false
-    }
-    // Add value to the array and map
-    this.arr.push(val);
-    this.map.set(val, this.arr.length - 1); // Store index in map
-    return true; // Successfully inserted
+/**
+ * Removes an item val from the set if present.
+ * Returns true if the item was present, false otherwise.
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.remove = function (val) {
+  // If the value is not present in the set (map), return false
+  if (!this.map.has(val)) {
+    return false;
   }
+  // Get the index of the value to be removed
+  const index = this.map.get(val);
+  // Get the last element in the array
+  const lastElement = this.arr[this.arr.length - 1];
 
-  // Removes a value from the set if present
-  remove(val) {
-    if (!this.map.has(val)) {
-      return false; // If value does not exist, return false
-    }
-    const index = this.map.get(val); // Get index of the value to remove
-    const lastElement = this.arr[this.arr.length - 1]; // Get the last element
+  // Swap the value to be removed with the last element in the array
+  this.arr[index] = lastElement;
+  // Update the map to reflect the new index of the last element
+  this.map.set(lastElement, index);
 
-    // Swap the value to be removed with the last element
-    this.arr[index] = lastElement;
-    this.map.set(lastElement, index); // Update map with new index for the swapped element
+  // Remove the last element from the array (which is now the value to be removed)
+  this.arr.pop();
+  // Remove the value from the map
+  this.map.delete(val);
 
-    // Remove the last element
-    this.arr.pop(); // Remove last element from array
-    this.map.delete(val); // Remove value from the map
-    return true; // Successfully removed
-  }
+  return true; // Return true to indicate the value was successfully removed
+};
 
-  // Returns a random element from the set
-  getRandom() {
-    const randomIndex = Math.floor(Math.random() * this.arr.length);
-    return this.arr[randomIndex]; // Return the element at the random index
-  }
-}
+/**
+ * Returns a random element from the current set of elements.
+ * Each element must have the same probability of being returned.
+ * @return {number}
+ */
+RandomizedSet.prototype.getRandom = function () {
+  // Generate a random index between 0 and the length of the array
+  const randomIndex = Math.floor(Math.random() * this.arr.length);
+  // Return the element at the random index
+  return this.arr[randomIndex];
+};
+
+/**
+ * Example usage:
+ * var obj = new RandomizedSet()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
