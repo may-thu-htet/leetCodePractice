abddddafabdb797f008ef0074cf094c10d9a4980
May Thu Htet <maythuhtet2892@gmail.com>
2025-03-20 17:06:23 +0000

    70 edit

--- recursion/70.Climbing stairs.js
+++ recursion/70.Climbing stairs.js
@@ -3,9 +3,13 @@
  * @return {number}
  */
 var climbStairs = function (n) {
-  function dfs(steps) {
-    if (steps >= n) return steps == n;
-    return dfs(steps + 1) + dfs(steps + 2);
+  if (n <= 2) return n;
+  let one = 1,
+    two = 1;
+  for (let i = 2; i <= n; i++) {
+    let temp = one;
+    one = one + two;
+    two = temp;
   }
-  return dfs(0);
+  return one;
 };
